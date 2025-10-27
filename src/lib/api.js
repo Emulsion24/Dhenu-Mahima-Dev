76537eacfdf5c31831bfae9a535fa2b5c11793c4
Shavandeb Kaiti <shavandebkaiti1024@gmail.com>
2025-10-27 19:11:16 +0530

    minor changes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,8 @@
-// src/lib/api.js
+// src/lib/api.j
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://saccharic-noncollusively-loni.ngrok-free.dev', // backend URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://saccharic-noncollusively-loni.ngrok-free.dev/api', // backend URL
   withCredentials: true,    
    headers: {
     "ngrok-skip-browser-warning": "true",
