cd7c879281b5c2708e291e63957d19114f1fab33
Shavandeb Kaiti <shavandebkaiti1024@gmail.com>
2025-10-28 15:52:05 +0530

    env miss match

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,15 +1,14 @@
-// src/lib/api.j
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
-const URL=process.env.API_URL;
-console.log(URL)
+
+const URL = process.env.NEXT_PUBLIC_API_URL;
+console.log("API_URL =>", URL);
+
 const API = axios.create({
-  baseURL:`${URL}`, // backend URL
-  withCredentials: true,    
-   headers: {
+  baseURL: URL,
+  withCredentials: true,
+  headers: {
     "ngrok-skip-browser-warning": "true",
-  },           
+  },
 });
 
 export default API;
