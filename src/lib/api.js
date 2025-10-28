cebc151b836ecdc99427cc82523adb918a683fa4
Shavandeb Kaiti <shavandebkaiti1024@gmail.com>
2025-10-28 15:41:19 +0530

    config isuue

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,8 +2,10 @@
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
+const URL=process.env.API_URL;
+console.log(URL)
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // backend URL
+  baseURL:`${URL}`, // backend URL
   withCredentials: true,    
    headers: {
     "ngrok-skip-browser-warning": "true",
